8b29d26d55abac7fd6a575a7b159a1a3998098ca
kleelof <kleelof@yahoo.com>
2019-11-21 13:51:38 -0800

    Added Game Loop Added Splash Screens Monsters Moving Clean up unnec files

--- script.js
+++ script.js
@@ -1,24 +1,27 @@
-var gameBoard = document.getElementById('game_board');
+var gameMode = 'not_started';
+
 var max = {
-    piece: "max",
-    left: 1,
-    top: 1,
+    x: 1,
+    y: 1,
     dom: document.getElementById('max'),
     direction: null
 };
-var score = 0;
-var gameMode = '';
-var diamondCount = 0;
+
+var gameLoopSpeed = 1;
+var maxsSpeed = 100; // for maxsSpeed and monsterSpeed, the higher the number, the slower they move.
+var monsterSpeed = 150;
+
+var monsters = [ makeMonster('monster_0'), makeMonster('monster_1'), makeMonster('monster_2') ];
+var monsterPursuitAccuracy = 9; // each time a monster moves, this is the chance they will move toward max. Higher number is more accurate.
+//todo: improve monster pursuit accuracy
+var loopTimer, level, score, diamondCount, lives, loopCounter;
+
+var gameBoard = document.getElementById('game_board');
+var blankSquares = [];
 var gameBoardPosition = {
     top: document.getElementById('game_board').offsetTop,
     left: document.getElementById('game_board').offsetLeft
 };
-
-var gameLoopSpeed = 200;
-var monsters = [];
-var monstersDom = document.getElementById('monsters');
-var level = 1;
-var blankSquares = [];
 var map = [
     [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
     [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
@@ -40,7 +43,6 @@ var map = [
     [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
     [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1]
 ];// 1 = wall, 2 = blue diamond, 3 = rainbow diamond
-
 var mapTemplate =
     "\t\t\t\t\t\t\tx\t\t\t\t\t\t\tx\t\t\n" +
     "\tx\tx\tx\t\tx\t\tx\t\tx\tx\tx\tx\t\tx\tx\t\n" +
@@ -61,27 +63,109 @@ var mapTemplate =
     "\t\t\t\t\tx\t\t\t\t\t\t\t\t\t\t\tx"
 ;
 
-importMapTemplate();
-resetBoard();
-startPlay();
+start();
+
+/* flow control */
+function start(){
+    document.getElementById('start_splash_screen').style.visibility = 'visible';
+    resetGame();
+    setTimeout(removeSplashScreen, 5000);
+}
+
+function removeSplashScreen(){
+    document.getElementById('start_splash_screen').style.visibility = 'hidden';
+    gotoNextLevel();
+}
+
+function resetGame(){
+    level = 0;
+    lives = 3;
+    score = 0;
+
+    importMapTemplate();
+    drawScoreboard();
+    addDiamonds();
+    drawboard();
+    addMonsters();
+    drawMovingThing(max);
+}
+
+function gotoNextLevel(){
+    level ++;
+    monsterSpeed -= 10;
+
+    addDiamonds();
+    drawboard();
+    resetLevel();
+    displayLevelScreen();
+}
+
+function resetLevel(){
+    addMonsters();
+    max.x = 1;
+    max.y = 1;
+    drawMovingThing(max);
+}
+
+function displayLevelScreen(){
+    document.getElementById('level_text').innerText = level;
+    document.getElementById('level_screen').style.visibility = 'visible';
+    setTimeout(getReadyToPlay, 3000);
+}
+
+function levelCleared(){
+    stopLoopTimer();
+    document.getElementById('level_cleared').style.visibility = 'visible';
+    setTimeout(clearLevelCleared, 3000);
+}
+
+function clearLevelCleared(){
+    document.getElementById('level_cleared').style.visibility = 'hidden';
+    gotoNextLevel();
+}
+
+function getReadyToPlay(){
+    document.getElementById('level_screen').style.visibility = 'hidden';
+    setTimeout(startPlay, 500);
+}
 
 function startPlay(){
-    drawboard();
-    drawMovingThing(max);
+    startLoopTimer();
+}
+
+function gameOver(){
+    document.getElementById('game_over').style.visibility = 'visible';
+}
+
+function gameLoop(){
+    loopCounter ++;
+    //if(loopCounter % maxsSpeed === 0 ) movemax();
+    //todo: improve Maxs speed
+    if(loopCounter % monsterSpeed === 0 ) moveMonsters();
+}
+
+function startLoopTimer(){
     gameMode='playing';
-    startTimer();
+    loopCounter = 0;
+    loopTimer = setInterval(gameLoop, gameLoopSpeed);
+}
+
+function stopLoopTimer(){
+    clearInterval(loopTimer);
 }
 
 document.onkeydown = function(e){
 
     if (gameMode !== 'playing') return;
     if ( e.keyCode > 36 && e.keyCode < 41 ) max.direction = e.keyCode;
+    movemax();
 };
 
 document.onkeyup = function(e){
     max.direction = null;
 };
 
+/* gameboard */
 function importMapTemplate(){
     var template = mapTemplate.split("\n").join("\t");
     template = template.split("\t");
@@ -110,20 +194,42 @@ function drawboard(){
     gameBoard.innerHTML = boardHTML;
 }
 
-function resetBoard(){
-    drawboard();
-    addDiamonds();
-    score = 0;
-    drawScoreboard();
-    addMonsters();
+function getSquareInfo( movingThing, tX, tY ){
+    var hasMonsters = monsters.filter(function( monster ){
+        if ( monster.top === tY + movingThing.top && monster.left === tX + movingThing.left ) return monster;
+    });
+
+    return {
+        canMoveTo: map[ tY + movingThing.y ][ tX + movingThing.x ] !== 1,
+        hasDiamond: ( map[ tY + movingThing.y ][ tX + movingThing.x ] === 2 ||
+            map[ tY + movingThing.y ][ tX + movingThing.x ] === 3 )?
+                map[ tY + movingThing.y ][ tX + movingThing.x ]: 0,
+        hasMonster: hasMonsters.length > 0,
+        x: tX + movingThing.x,
+        y: tY + movingThing.y,
+        tX: tX,
+        tY: tY
+    };
 }
+
+function drawScoreboard(){
+    document.getElementById("score").innerText = score;
+
+    var livesPanelDOM = document.getElementById('lives');
+    livesPanelDOM.innerHTML = "";
+    for( var i = 0; i < lives; i ++ ){
+        livesPanelDOM.innerHTML += `<img src="images/max.png" class="max_lives">`;
+    }
+}
+
+/* diamonds */
 
 function addDiamonds(){
     diamondCount = 0;
     blankSquares = [];
     for(var y = 0; y < map.length; y ++ ){
         for(var x = 0; x < map[0].length; x ++ ){
-            if (map[y][x] === 0) {
+            if (map[y][x] !== 1) {
                 map[y][x] = (Math.floor(Math.random() * 100) <5)? 3 : 2;
                 diamondCount ++;
                 blankSquares.push({x: x, y: y})
@@ -133,61 +239,77 @@ function addDiamonds(){
 }
 
 function countDiamond( x, y ){
-    var diamondType =
     score += (map[y][x] === 2)? 5:20;
     map[y][x] = 0;
     drawboard();
     drawScoreboard();
+
+    diamondCount --;
+    if ( diamondCount === 0 ) levelCleared();
 }
 
-/* add 2 monsters + 1 for each level */
-function addMonsters(){
-    monstersDom.innerHTML = "";
-    var square, newMonster;
-    for( var i = 0; i < 2 + level; i ++ ){
-        square = blankSquares[ Math.floor(Math.random() * blankSquares.length) ];
-        monstersDom.innerHTML += "" +
-            "<div id='monster_" + monsters.length + "' class='monster'>" +
-            "<img src='images/monster_" + Math.floor(Math.random() * 2) + ".png'>" +
-            "</div>";
-        newMonster = {
-            piece: 'monster',
-            top: square.y,
-            left: square.x,
-            dom: document.getElementById('monster_' + monsters.length),
-            direction: null
-        };
-        monsters.push(newMonster);
-        drawMovingThing(newMonster);
+/* monsters */
+function makeMonster( id ){
+    return {
+        dom: document.getElementById(id),
+        x: 0,
+        y: 0,
+        last_square: {
+            x: 0,
+            y: 0
+        }
     }
-    console.log(monsters)
+}
+
+function addMonsters(){console.log(blankSquares);
+    var square;
+    for( var i = 0; i < monsters.length; i ++ ){
+        square = blankSquares[ Math.floor(Math.random() * blankSquares.length) ];
+        monsters[i].y = square.y;
+        monsters[i].x = square.x;
+        drawMovingThing(monsters[i]);
+    }
 }
 
 function moveMonsters(){
-    var tX = 0;
-    var tY = 0;
-    var testedSquareValue = 1;
-    var monster
-    for( var i = 0; i < monsters.length; i ++ ){
-        monster = monsters[i];
-        while( testedSquareValue === 1 ){
-            switch(Math.floor(Math.random() * 3)){// select a direction
-                case 0: tY = -1; break; //up
-                case 1: tX = -1; break; //left
-                case 2: tY = 1; break; //down
-                case 3: tX = 1; break; //right
-            }
-
-            testedSquareValue = testSquare(monsters[i], tX, tY)
-        }
-        //moveMovingThing(monsters[i], tX, tY );
+    var square;
+    for( var i = 0; i < monsters.length; i ++ ) {
+        monsters[i].last_square = { x: monsters[i].x, y: monsters[i].y };// prevents monsters from getting stuck when pursuing Max.
+        square = getSquareForMonsterToMoveTo( monsters[i], Math.floor(Math.random() * 9) < monsterPursuitAccuracy);
+        moveMovingThing(monsters[i], square.tX, square.tY);
     }
+    if(shouldMaxDie()) maxHasDied();
 }
 
-function drawScoreboard(){
-    document.getElementById("score").innerText = score;
+function getSquareForMonsterToMoveTo( monster, moveTowardMax ){
+    // get 4 compass point directions
+    var possibleSquares = [
+        getSquareInfo(monster, -1, 0),
+        getSquareInfo(monster, 1, 0),
+        getSquareInfo(monster, 0, -1),
+        getSquareInfo(monster, 0, 1)
+    ];
+
+    // remove squares that cannot be moved to.
+    possibleSquares = possibleSquares.filter(function(square){
+        if(square.canMoveTo) return square;
+    });
+
+    if(moveTowardMax) {
+        var possibleSquaresTowardMax = possibleSquares.filter(function (square) {
+            if (max.x < square.x && square.x < monster.x && (monster.last_square.x !== square.x && monster.last_square.y !== square.y)) return square;
+            if (max.x > square.x && square.x > monster.x && (monster.last_square.x !== square.x && monster.last_square.y !== square.y)) return square;
+            if (max.y < square.y && square.y < monster.y && (monster.last_square.x !== square.x && monster.last_square.y !== square.y)) return square;
+            if (max.y > square.y && square.y > monster.y && (monster.last_square.x !== square.x && monster.last_square.y !== square.y)) return square;
+        });
+        possibleSquares = (possibleSquaresTowardMax.length > 0)? possibleSquaresTowardMax:possibleSquares;
+    }
+
+    return possibleSquares[ Math.floor(Math.random() * possibleSquares.length)];
 }
 
+
+/* Max */
 function movemax(){
     if ( max.direction === null ) return;
 
@@ -201,36 +323,48 @@ function movemax(){
         case 37: tX = -1; break;
     }
 
-    var nextSquare = testSquare(max, tX, tY);
-    if (nextSquare === 0){
-        moveMovingThing( max, tX, tY)
-    }else if (nextSquare > 1 && nextSquare < 4){
-        moveMovingThing( max, tX, tY);
-        countDiamond( max.left, max.top );
+    var nextSquare = getSquareInfo(max, tX, tY);
+    if(nextSquare.canMoveTo) moveMovingThing( max, tX, tY);
+    if(nextSquare.hasMonster){
+        maxHasDied();
+        return;
+    }
+    if(nextSquare.hasDiamond > 0) countDiamond(max.x, max.y);
+    if(shouldMaxDie()) maxHasDied();
+}
+
+function shouldMaxDie(){
+    return monsters.filter(function(monster){
+        if (monster.x === max.x && monster.y === max.y) return monster;
+    }).length > 0;
+}
+
+function maxHasDied(){
+    stopLoopTimer();
+    gameMode = 'maxDied';
+    lives --;
+    drawScoreboard();
+
+    if( lives > 0 ){
+        resetLevel();
+        displayLevelScreen();
+    }else{
+       gameOver();
     }
 }
 
+/* movingThings */
 function moveMovingThing( movingThing, x, y){
-    movingThing.top += y;
-    movingThing.left += x;
+    movingThing.y += y;
+    movingThing.x += x;
     drawMovingThing(movingThing);
 }
 
 function drawMovingThing( movingThing ){
-    movingThing.dom.style.top = gameBoardPosition.top + (movingThing.top * 40) + 'px';
-    movingThing.dom.style.left = gameBoardPosition.left +  (movingThing.left * 40) + 'px';
+    movingThing.dom.style.top = gameBoardPosition.top + (movingThing.y * 40) + 'px';
+    movingThing.dom.style.left = gameBoardPosition.left +  (movingThing.x * 40) + 'px';
 }
 
-function testSquare( movingThing, tX, tY ){
-    return map[ tY + movingThing.top ][ tX + movingThing.left ];
-}
 
-function gameLoop(){
-    movemax();
-    moveMonsters();
-}
 
-function startTimer(){
-    if( gameMode === "playing" ) setInterval(gameLoop, gameLoopSpeed);
-}
 
